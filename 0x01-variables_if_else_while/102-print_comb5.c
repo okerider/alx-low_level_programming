3ccfaf16eb7aa7c14afb746ef77910a2123cab88
okerider <okerider@gmail.com>
2022-09-09 10:17:34 -0700

    Remove trailing whitespace

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,7 +9,6 @@ int main(void)
 {
 	int i;
 	int j;
-	
 	for (i = 0; i <= 99; i++)
 	{
 		for (j = i + 1; j <= 99; j++)
@@ -19,6 +18,7 @@ int main(void)
 			putchar(' ');
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
+			
 			if (!(i == 98 && j == 99))
 			{
 				putchar(',');
