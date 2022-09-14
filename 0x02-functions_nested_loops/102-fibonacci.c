f38b1327c09caa3612c15d88c6ccb30f212a4466
okerider <okerider@gmail.com>
2022-09-14 16:02:10 -0700

    102 fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,21 +7,19 @@
 int main(void)
 {
 	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
 
-	float a = 1;
-	float b = a + 1;
-	float c = a + b;
-
-	printf("%.0f, ", a);
-	printf("%.0f, ", b);
-	while (counter < 98)
+	printf("%ld, %ld, ", a, b);
+	while (counter < 50)
 	{
+		printf("%ld", c);
 		counter++;
-		printf("%.0f", c);
 		a = b;
 		b = c;
 		c = a + b;
-		if (counter < 98)
+		if (counter < 50)
 		{
 			printf(", ");
 		}
